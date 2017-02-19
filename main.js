ed868cca745150e3493dbfeb92c682991bfbba2f
gitseo <gitseo@ya.ru>
2017-02-20 02:10:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
-
 var qs = require('querystring');
 var g_obj={};
 process.on('uncaughtException',err=>console.log(err));
@@ -20,6 +19,12 @@ process.on('uncaughtException',err=>console.log(err));
 var json=s=>JSON.stringify(s);
 var mapkeys=Object.keys;var mapvals=Object.values;
 var inc=(m,k)=>{if(!(k in m))m[k]=0;m[k]++;return m[k];};
+");
+
+var getarr=(m,k)=>{if(!(k in m))m[k]=[];return m[k];};
+var getmap=(m,k)=>{if(!(k in m))m[k]={};return m[k];};
+
+var g_devtest=()=>{return 'nothing';}
 
 var http_server=http.createServer((a,b)=>{return requestListener(a,b);}).listen(port,ip);
 var requestListener=(request, response)=>{
@@ -45,6 +50,29 @@ var requestListener=(request, response)=>{
     var is_dir=fn=>fs.statSync(filename).isDirectory();
     fs.exists(filename,ok=>{if(ok&&is_dir(filename))filename+='/index.html';func(filename);});
     var func=filename=>fs.exists(filename,function(exists) {
+      var txt=(s)=>{response.writeHead(200,{"Content-Type":"text/plain"});response.end(s);}
+      if("/put"==uri){
+        var qp=qs.parse(url.parse(request.url).query);
+        getmap(g_obj,'files')[qp.fn]=qp.data;
+        txt(json(qp));
+        return;
+      }
+      if("/get"==uri){
+        var qp=qs.parse(url.parse(request.url).query);
+        txt(getmap(g_obj,'files')[qp.fn]);
+        return;
+      }
+      if("/list"==uri){
+        var qp=qs.parse(url.parse(request.url).query);
+        txt(mapkeys(getmap(g_obj,'files')).join("\n"));
+        return;
+      }
+      if("/test"==uri){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(g_devtest);
+        response.end();
+        return;
+      }
       if("/"==uri){
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("count="+inc(g_obj,'counter'));
