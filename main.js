3439a81b4a9e2e614e6646e4e07e950078566f13
gitseo <gitseo@ya.ru>
2017-02-20 23:38:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ var http = require("http"),
     path = require("path"),
     fs = require("fs");
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
