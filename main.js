cd2e3854baac8d53e3fa6962cf0a232196d6fe33
gitseo <gitseo@ya.ru>
2017-02-22 23:22:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ var hosts={};var hosts_err_msg='';var need_coop_init=true;
 
 var hosts_sync=(cb)=>{
   if(typeof cb=='undefined')cb=()=>{};
-  xhr_post('https://raw.githubusercontent.com/adler3d/qap_vm/gh-pages/trash/test2017/hosts.json',{},s=>{hosts=JSON.parse(s);cb(s);},s=>{hosts_err_msg=s;cb(s);});
+  xhe_get('https://raw.githubusercontent.com/adler3d/qap_vm/gh-pages/trash/test2017/hosts.json?t='+rand(),s=>{hosts=JSON.parse(s);cb(s);},s=>{hosts_err_msg=s;cb(s);});
 };
 
 hosts_sync();
