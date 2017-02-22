3c55728836d7a9ada7af0f5e47f90f68bd197f19
gitseo <gitseo@ya.ru>
2017-02-22 18:41:03 +0300

    Create mainloop.js

---
+++ mainloop.js
@@ -0,0 +1,8 @@
+var exec=require('child_process').exec;
+var file_exist=fn=>{try{require('fs').accessSync(fn);return true;}catch(e){return false;}}
+var fn="fast_unsafe_auto_restart_enabled.txt";
+exec("echo created inside mainloop.js>"+fn);
+for(;;)
+  exec("node main.js")
+  if(!file_exist(fn))break;
+}
