e13ce54fde99e6147e32ad300d0a71d89b6f6873
gitseo <gitseo@ya.ru>
2017-02-22 19:20:27 +0300

    Update mainloop.js

--- mainloop.js
+++ mainloop.js
@@ -2,7 +2,7 @@ var exec=require('child_process').exec;
 var file_exist=fn=>{try{require('fs').accessSync(fn);return true;}catch(e){return false;}}
 var fn="fast_unsafe_auto_restart_enabled.txt";
 exec("echo created inside mainloop.js>"+fn);
-for(;;)
+for(;;){
   exec("node main.js")
   if(!file_exist(fn))break;
 }
