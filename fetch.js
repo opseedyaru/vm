04afc438281c0b8e062d732c176fcc4219028f79
adler3d <adler3d@gmail.com>
2017-02-24 17:25:10 +0300

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,25 @@
+var http = require("http"),
+    https = require("https"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    os = require("os");
+
+var xhr_get=(url,ok,err)=>{
+  var req=(url.substr(0,"https".length)=="https"?https:http).get(url,(res)=>{
+    var statusCode=res.statusCode;var contentType=res.headers['content-type'];var error;
+    if(statusCode!==200){error=new Error('Request Failed.\nStatus Code: '+statusCode);}
+    if(error){err(error.message);res.resume();return;}
+    //res.setEncoding('utf8');
+    var rawData='';res.on('data',(chunk)=>rawData+=chunk);
+    res.on('end',()=>{try{ok(rawData);}catch(e){err(e.message);}});
+  }).on('error',(e)=>{err('Got error: '+e.message);});
+  return req;
+}
+
+var repo="https://raw.githubusercontent.com/gitseo/vm/master/";
+var fn=('fn' in qp)?qp[fn]:"main.js";
+xhr_get(repo+fn+'?t='+rand(),s=>{
+  fs.writeFileSync(fn,s);
+  console.log("done!\nlength = "+Buffer.byteLength(s));
+},console.error);
