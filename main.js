394267e659a129de9d68c485ab8a751be3f355c2
adler3d <adler3d@gmail.com>
2017-06-14 14:16:37 +0400

    Update main.js

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ var xhr_get=(url,ok,err)=>{
 var xhr=(method,URL,data,ok,err)=>{
   var up=url.parse(URL);var secure=up.protocol=='https';
   var options={
-    hostname:up.host,port:secure?443:80,path:up.path,method:method.toUpperCase(),
+    hostname:up.hostname,port:up.port?up.port:(secure?443:80),path:up.path,method:method.toUpperCase(),
     headers:{'Content-Type':'application/x-www-form-urlencoded','Content-Length':Buffer.byteLength(data)}
   };
   var req=(secure?https:http).request(options,(res)=>{
