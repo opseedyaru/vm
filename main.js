3b0ce717e657f71ac5c0ad097cfc5a8f15c7f686
adler3d <adler3d@gmail.com>
2017-08-11 20:06:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -356,6 +356,7 @@ var requestListener=(request, response)=>{
           var func=need_png?png:txt;
           if('binary' in qp)func=binary;
           if('bin' in qp)func=binary;
+          if('html' in qp)func=html;
           if('txtbin' in qp)func=txtbin;
           return coop(
             (arr,log_object)=>func(
