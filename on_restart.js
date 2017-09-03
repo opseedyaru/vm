7a2f5592711955d64a49fedca19c05808ae0d323
adler3d <adler3d@gmail.com>
2017-09-03 18:53:47 +0300

    Update on_restart.js

--- on_restart.js
+++ on_restart.js
@@ -1,4 +1,5 @@
 start_auto_backup();
 set_interval(()=>xhr_get('http://qpe.000webhostapp.com/vm/ping?from='+os.hostname(),()=>{},()=>{}),60*1000);
+var nope=()=>{};
 xhr_get('http://qpe.000webhostapp.com/vm/on_start?from='+os.hostname(),nope,nope);
 xhr_get('http://adler.hol.es/vm/on_start?from='+os.hostname(),nope,nope);
