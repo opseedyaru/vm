46dacbab8616b79f52fe1c2a945f40b8c2d24df1
adler3d <adler3d@gmail.com>
2017-09-05 13:10:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -81,13 +81,13 @@ function getDateTime() {
   return dateTime;
 }
 
-var cl_and_exec_cpp=(code,async_cb)=>{
+var cl_and_exec_cpp=(code,async_cb,flags)=>{
   var rnd=rand()+"";rnd="00000".substr(rnd.length)+rnd;
   var fn="main["+getDateTime().split(":").join("-").split(" ").join("_")+"]_"+rnd+".cpp";
   var out="./"+fn+".out";
   //fn=json(fn);out=json(out);
   fs.writeFileSync(fn,code);
-  var cmdline="g++ -std=c++11 "+fn+" -O2 -o "+out+"\n"+out;
+  var cmdline="g++ "+(flags?flags:"")+"-Wmultichar -fpermissive -DQAP_DEBUG -std=c++11 "+fn+" -O2 -o "+out+"\n"+out;
   if(async_cb){
     if((typeof async_cb)!="function")async_cb=()=>{};
     var proc=exec(cmdline,async_cb);
@@ -252,7 +252,15 @@ var requestListener=(request, response)=>{
       var txtbin=((res)=>{var r=res;return s=>{r.writeHead(200,{"Content-Type":"text/plain"});r.end(new Buffer(s,"binary"));}})(response);
       var html=((res)=>{var r=res;return s=>{r.writeHead(200,{"Content-Type":"text/html"});r.end(s);}})(response);
       var txt=((res)=>{var r=res;return s=>{r.writeHead(200,{"Content-Type":"text/plain"});r.end(s);}})(response);
-      var async_cl_and_exec_cpp=(code)=>cl_and_exec_cpp(code,(error,stdout,stderr)=>txt("so...\n"+(error?"err: "+inspect(error)+"\n"+stderr:"ok: "+(stdout))));
+      var async_cl_and_exec_cpp=(code,flags)=>{
+        cl_and_exec_cpp(
+          code,
+          (error,stdout,stderr)=>txt(
+            "so...\n"+(error?"err: "+inspect(error)+"\n"+stderr+"\n\n ***--- stdout: ---*** \n\n"+stdout:"ok: "+(stdout))
+          ),
+          flags
+        );
+      }
       var shadow=mapkeys(hosts)[mapvals(hosts).indexOf('shadow')];
       var shadows=mapkeys(hosts).filter(e=>hosts[e]==('shadow'));
       var master=mapkeys(hosts)[mapvals(hosts).indexOf('public')];
