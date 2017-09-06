6cf4948a0df064d322539d30f3beae70f15e89be
adler3d <adler3d@gmail.com>
2017-09-06 12:05:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ var send_backup=()=>{
   );
 }
 
-var g_intervals=[]; var g_head_content_length=true;
+var g_intervals=[];
 
 var set_interval=(func,ms)=>{
   g_intervals.push({data:getDateTime(),func:func,ref:setInterval(func,ms)});
@@ -504,19 +504,13 @@ var requestListener=(request, response)=>{
           response.end("404 Not Found\n");
           return;
         }
-        fs.readFile(filename,"binary",function(err,file){
-          if(err){
-            response.writeHead(500,{"Content-Type":"text/plain"});
-            response.end(err+"\n");
-            return;
-          }
-          var headers={};
-          var contentType=contentTypesByExtension[path.extname(filename)];
-          if(contentType)headers["Content-Type"]=contentType;
-          if(g_head_content_length)headers['Content-Length']=file.length;
-          response.writeHead(200,headers);
-          response.write(file,"binary");
-          response.end();
+        fs.stat(filename,(error,stat)=>{
+          if(error){throw error;}
+          response.writeHead(200,{
+            'Content-Type':contentTypesByExtension[path.extname(filename)],
+            'Content-Length':stat.size
+          })
+          fs.createReadStream(filename).pipe(response);
         });
       };
       if(need_coop_init){
