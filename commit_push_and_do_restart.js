cf880bdbe01e090acf7c511421637c0fa951dc66
Adler3D <adler3d@gmail.com>
2017-09-09 14:05:11 +0300

    upd

---
+++ commit_push_and_do_restart.js
@@ -0,0 +1,191 @@
+var child_process=require('child_process');
+var fs=require('fs');
+var execSync=child_process.execSync;
+var spawn=child_process.spawn;
+var qs = require('querystring');
+var http = require("http"),
+    https = require("https"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    os = require("os"),
+crypto = require('crypto');
+
+var QapNoWay=()=>{qap_log("QapNoWay :: no impl");qap_log("no way");}
+
+var call_cb_on_err=(emitter,cb,...args)=>{
+  emitter.on('error',err=>{
+    cb("'inspect({args,err}) // stack': "+inspect({args:args,err:err})+" // "+err.stack.toString());
+  });
+}
+
+var qap_err=(context,err)=>context+" :: err = "+inspect(err)+" //"+err.stack.toString();
+var log_err=(context,err)=>qap_log(qap_err(context,err));
+
+process.on('uncaughtException',err=>log_err('uncaughtException',err));
+
+var rand=()=>(Math.random()*1024*64|0);
+var qap_add_time=s=>"["+getDateTime()+"] "+s;
+var qap_log=s=>console.log(qap_add_time(s));
+
+var json=JSON.stringify;
+var mapkeys=Object.keys;var mapvals=(m)=>mapkeys(m).map(k=>m[k]);
+var inc=(m,k)=>{if(!(k in m))m[k]=0;m[k]++;return m[k];};
+
+var FToS=n=>(n+0).toFixed(2);
+var qapavg=(arr,cb)=>{if(typeof cb=='undefined')cb=e=>e;return arr.length?arr.reduce((pv,ex)=>pv+cb(ex),0)/arr.length:0;}
+var qapsum=(arr,cb)=>{if(typeof cb=='undefined')cb=e=>e;return arr.reduce((pv,ex)=>pv+cb(ex),0);}
+var qapmin=(arr,cb)=>{if(typeof cb=='undefined')cb=e=>e;var out;var i=0;for(var k in arr){var v=cb(arr[k]);if(!i){out=v;}i++;out=Math.min(out,v);}return out;}
+var qapmax=(arr,cb)=>{if(typeof cb=='undefined')cb=e=>e;var out;var i=0;for(var k in arr){var v=cb(arr[k]);if(!i){out=v;}i++;out=Math.max(out,v);}return out;}
+var qapsort=(arr,cb)=>{if(typeof cb=='undefined')cb=e=>e;return arr.sort((a,b)=>cb(b)-cb(a));}
+var mapdrop=(e,arr,n)=>{var out=n||{};Object.keys(e).map(k=>arr.indexOf(k)<0?out[k]=e[k]:0);return out;}
+var mapsort=(arr,cb)=>{if(typeof cb=='undefined')cb=(k,v)=>v;var out={};var tmp=qapsort(mapkeys(arr),k=>cb(k,arr[k]));for(var k in tmp)out[tmp[k]]=arr[tmp[k]];return out;}
+
+var qap_unique=arr=>{var tmp={};arr.map(e=>tmp[e]=1);return mapkeys(tmp);};var unique_arr=qap_unique;
+
+var mapaddfront=(obj,n)=>{for(var k in obj)n[k]=obj[k];return n;}
+var mapclone=obj=>mapaddfront(obj,{});
+
+var getarr=(m,k)=>{if(!(k in m))m[k]=[];return m[k];};
+var getmap=(m,k)=>{if(!(k in m))m[k]={};return m[k];};
+
+var json_once=(obj,replacer,indent,limit)=>{
+  var objs=[];var keys=[];if(typeof(limit)=='undefined')limit=2048;
+  return json(obj,(key,v)=>{
+    if(objs.length>limit)return 'object too long';
+    var id=-1;objs.forEach((e,i)=>{if(e===v){id=i;}});
+    if(key==''){objs.push(obj);keys.push("root");return v;}
+    if(id>=0){
+      return keys[id]=="root"?"(pointer to root)":
+        ("\1(see "+((!!v&&!!v.constructor)?v.constructor.name.toLowerCase():typeof(v))+" with key "+json(keys[id])+")");
+    }else{
+      if(v!==null&&typeof(v)==="object"){var qk=key||"(empty key)";objs.push(v);keys.push(qk);}
+      return replacer?replacer(key,v):v;
+    }
+  },indent);
+};
+var json_once_v2=(e,v,lim)=>json_once(e,v,2,lim);
+var inspect=json_once_v2;
+
+function getDateTime() {
+  var now     = new Date(); 
+  var year    = now.getFullYear();
+  var f=v=>(v.toString().length==1?'0':'')+v;
+  var month   = f(now.getMonth()+1); 
+  var day     = f(now.getDate());
+  var hour    = f(now.getHours());
+  var minute  = f(now.getMinutes());
+  var second  = f(now.getSeconds()); 
+  var dateTime = year+'.'+month+'.'+day+' '+hour+':'+minute+':'+second;   
+  return dateTime;
+}
+
+var file_exist=fn=>{try{fs.accessSync(fn);return true;}catch(e){return false;}}
+var rand=()=>(Math.random()*1024*64|0);
+
+var xhr_get=(url,ok,err)=>{
+  if((typeof ok)!="function")ok=()=>{};
+  if((typeof err)!="function")err=()=>{};
+  var req=(url.substr(0,"https".length)=="https"?https:http).get(url,(res)=>{
+    if(res.statusCode!==200){err('Request Failed.\nStatus Code: '+res.statusCode);res.destroy();req.destroy();return;}
+    //res.setEncoding('utf8');
+    var rawData='';res.on('data',(chunk)=>rawData+=chunk);
+    res.on('end',()=>{try{ok(rawData,res);}catch(e){err(e.message,res);}});
+  });
+  call_cb_on_err(req,qap_log,'xhr_get');
+  return req;
+}
+
+var xhr=(method,URL,data,ok,err)=>{
+  if((typeof ok)!="function")ok=()=>{};
+  if((typeof oerrk)!="function")err=()=>{};
+  var up=url.parse(URL);var secure=up.protocol=='https';
+  var options={
+    hostname:up.hostname,port:up.port?up.port:(secure?443:80),path:up.path,method:method.toUpperCase(),
+    headers:{'Content-Type':'application/x-www-form-urlencoded','Content-Length':Buffer.byteLength(data)}
+  };
+  var req=(secure?https:http).request(options,(res)=>{
+    if(res.statusCode!==200){err('Request Failed.\nStatus Code: '+res.statusCode);res.destroy();req.destroy();return;}
+    //res.setEncoding('utf8');
+    var rawData='';res.on('data',(chunk)=>rawData+=chunk.toString("binary"));
+    res.on('end',()=>{try{ok(rawData,res);}catch(e){err(e.message,res);}});
+  });
+  call_cb_on_err(req,qap_log,'xhr');
+  req.end(data);
+  return req;
+}
+
+var hosts=[
+  "http://vm-vm.1d35.starter-us-east-1.openshiftapps.com",
+  "http://agile-eyrie-44522.herokuapp.com",
+  "http://vm-vm.193b.starter-ca-central-1.openshiftapps.com"
+];
+
+var sh=spawn('sh.exe',['-c',"git commit -am 'upd'\ngit push"],{stdio:[0,1,2]});
+var u=event=>sh.on(event,e=>qap_log('sh :: Got '+event));
+'close,disconnect,error,exit,message'.split(',').map(u);
+sh.on('close',()=>xhr_get(hosts[2]+"/fetch?quit",qap_log,qap_log));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
