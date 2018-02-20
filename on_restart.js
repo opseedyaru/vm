3afd5c717575dc2cb7bdfd0c026e69cd226538b2
adler3d <adler3d@gmail.com>
2018-02-20 10:29:02 +0300

    Update on_restart.js

--- on_restart.js
+++ on_restart.js
@@ -15,7 +15,7 @@ set_interval(()=>get_hosts_by_type('backup').map(
 get_hosts_by_type('backup').map(e=>xhr_get('http://'+e+'/vm/on_start?from='+os.hostname(),nope,nope));
 xhr_get('http://adler.hol.es/vm/on_start?from='+os.hostname(),nope,nope);
 
-var fetch_other_file=()=>["eval.html"].map(fn=>xhr_get('https://raw.githubusercontent.com/gitseo/vm/master/'+fn+'?t='+rand(),
+var fetch_other_file=(files)=>files.map(fn=>xhr_get('https://raw.githubusercontent.com/gitseo/vm/master/'+fn+'?t='+rand(),
   data=>{
     qap_log("fetch :: "+fn+" :: ok //"+data.length);
     fs.writeFile(fn,data);
@@ -29,7 +29,7 @@ xhr_get('https://raw.githubusercontent.com/gitseo/vm/master/main.js?t='+rand(),
     if(fs.readFileSync("main.js")==s){qap_log("on_restart.js :: main.js is up-to-date");return;}
     qap_log("on_restart.js :: main.js is old");
     fs.writeFileSync("main.js",s);
-    //fetch_other_file();
+    fetch_other_file(["eval.html"]);
     process.exit();
   },
   s=>{qap_log("on_restart.js :: fail :: "+s);fs.writeFileSync("main.js.errmsg","//from on_restart.js\n"+s);}
