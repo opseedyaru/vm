d499180b33ce8d591fd9da6404d857cee820d424
adler3d <adler3d@gmail.com>
2018-02-28 02:51:20 +0300

    Update run_logging.js

--- crude/run_logging.js
+++ crude/run_logging.js
@@ -1,13 +1,14 @@
 if(!('sure' in qp))return 'sure in qp required';
 var counter=10000;
 if('counter' in qp)counter=qp.counter|0;
+var ids='ids' in qp?"&ids="+qp.ids:'';
 var r=request;
 var dir='./wmlogs';
 fs.mkdir("./wmlogs",err=>{
   exec_with_stream('ls '+dir,r,()=>{
     r.write('\n\n');
     set_interval(()=>{
-      xhr_get("http://vm-os3.7e14.starter-us-west-2.openshiftapps.com/c/wmlog.js?json",
+      xhr_get("http://vm-os3.7e14.starter-us-west-2.openshiftapps.com/c/wmlog.js?json"+ids,
         s=>{
           var fn=dir+'/'+counter+'.json';
           fs.writeFileSync(fn,s);
