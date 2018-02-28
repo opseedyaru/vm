67e82a08b6a0ce6f70f34a012bb6052f2a3f809f
Adler3D <adler3d@gmail.com>
2018-02-28 16:14:59 +0300

    upd

--- crude/test_promise.js
+++ crude/test_promise.js
@@ -1,16 +1,16 @@
 resp_off();
 let promise=new Promise((resolve,reject)=>{
   setTimeout(() => {
-    if(rand()%2==0)throw new Error('fail');
+    if(rand()%2==0)reject('fail');
     resolve("result");
   },16);
 });
 promise
   .then(
     result => {
-      txt("Fulfilled: " + result);
+      txt("Fulfilled: "+result);
     },
     err => {
-      txt("Rejected: " + err);
+      txt("Rejected: "+err);
     }
   );
