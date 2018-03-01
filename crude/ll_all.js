741aeb9f9c56333dab9892d101cafe9a12a1d64e
Adler3D <adler3d@gmail.com>
2018-03-01 17:54:09 +0300

    upd

--- crude/ll_all.js
+++ crude/ll_all.js
@@ -1,4 +1,5 @@
 resp_off();
+var c=g_conf_info;
 Promise.all(
-  mapkeys(g_conf_info.wm_ids_src).map(e=>"http://"+c.vh2host[e]+"/ll").map(axhr_get)
+  mapkeys(c.wm_ids_src).map(e=>"http://"+c.vh2host[e]+"/ll").map(e=>axhr_get(e).then())
 ).then(arr=>jstable(arr.map(e=>{return {data:e}}))).catch(err=>txt('promise.all failed with: '+inspect(err)));
