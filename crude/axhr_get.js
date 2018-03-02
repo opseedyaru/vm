374adbbbf19d60b8c5f90ada598575c2e3d95f87
Adler3D <adler3d@gmail.com>
2018-03-02 11:25:33 +0300

    upd

--- crude/axhr_get.js
+++ crude/axhr_get.js
@@ -1,7 +1,7 @@
-var a=g_conf_info.wm_ids_src;
-if('show_links' in qp)return mapkeys(a).map(e=>"http://"+c.vh2host[e]+"/wmlogs.all.txt.tgz").join("\n");
+var c=g_conf_info;
 resp_off();
-var code=encodeURIComponent(
+//var code=POST.data;
+var code=(
   (()=>{
     var trash=[];
     var check=(fn)=>{
@@ -18,7 +18,20 @@ var code=encodeURIComponent(
     return txt(inspect(trash)+get_ms()+'');
   }).toString().split("\n").slice(1,-1).join("\n")
 );
-var path="/eval?&nolog&code="+code;
-Promise.all(
-  mapkeys(a).map(e=>axhr_get("http://"+c.vh2host[e]+path,c.vh2host[e]))
-).then(arr=>jstable(arr.map(e=>{return {host:e.ud,data:e.data}}))).catch(err=>txt('promise.all failed with: '+inspect(err)));
+var path="/eval?&nolog";
+var out_func=jstable;
+if('json' in qp)out_func=arr=>txt(json(arr));
+var isObject=a=>!!a&&a.constructor===Object;
+safe_promise_all(
+  mapkeys(c.vh2host).map(e=>{
+    var url="http://"+c.vh2host[e]+path;
+    var obj={unixtime:unixtime(),code:code};
+    var ud={vhost:e,host:c.vh2host[e],bef_ms:get_ms()};
+    return new Promise((ok,err)=>xhr_post(url,obj,
+      s=>ok({url:url,obj:obj,ud:ud,data:s,err:false}),
+      s=>ok({url:url,obj:obj,ud:ud,data:s,err:true})
+    )).then(e=>mapaddfront(e,{ms:get_ms()-e.ud.bef_ms}));
+  })
+).then(arr=>out_func(arr.map(e=>{
+  return {vhost:e.ud.vhost,host:e.ud.host,ms:e.ms.toFixed(3),data:e.data,error:e.err}
+})));
