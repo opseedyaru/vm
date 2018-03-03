2c415c9d20a0a9ab97b2d13deeb06803b60a1437
Adler3D <adler3d@gmail.com>
2018-03-03 20:39:05 +0300

    jstable_right win, pretty func are deleted

--- crude/wmlog.js
+++ crude/wmlog.js
@@ -6,6 +6,7 @@ var d=s=>parseFloat(s.split(",").join("."));
 var div=(a,b)=>d(a)/d(b);
 var out_in=(out,e)=>{out['out/in']=div(e.amountout,e.amountin).toFixed(3)};
 var in_out=(out,e)=>{out['in/out']=div(e.amountin,e.amountout).toFixed(3)};
+//qp.json=1;
 if(uri==='/eval')qp.profit=1;
 var white_list='Z,R,X,E,B,G';
 var dir2str={
@@ -38,12 +39,12 @@ var check_done=()=>{
   }
   if('json' in qp){return txt(json(tables));}
   var t1=tables[ids_arr[0]];
-  if(ids_arr.length==1)return jstable(t1);
+  if(ids_arr.length==1)return jstable_right(t1);
   if(ids_arr.length==2)
   {
     var t2=tables[ids_arr[1]];
     var t2_keys=mapkeys(t2[0]).reverse();
-    jstable(t1.map((e,i)=>{
+    jstable_right(t1.map((e,i)=>{
       t2_keys.map(k=>e[k+'`']=t2[i][k]);
       return e;
     }));
@@ -55,16 +56,7 @@ var run=exchtype=>{
   var insert_special_field=exchtype%2==type2dir(exchtype)?out_in:in_out;
   var ok=xml=>{
     var g=obj=>{tables[exchtype]=obj;check_done();};//0?(obj=>txt(inspect(obj))):obj=>jstable(obj);
-    var pretty=s=>{
-      var a=s.split('.');
-      var n=a[0].length;var out="";
-      for(var i=0;i<n;i++){
-        out=a[0][n-1-i]+out;
-        //if((i%3==2)&&(i+1!=n))out=' '+out;
-      }
-      a[0]=out;
-      return '         '.slice(0,-a[0].length)+a.join('.');
-    };
+    var pretty=s=>s;
     var select=(e,arr)=>{var out={};arr.split(',').map((k,i)=>out[k]=(i==2||i==3)?pretty(d(e[k]).toFixed(2)):e[k]);insert_special_field(out,e);return out;};
     var f=obj=>obj["wm.exchanger.response"]["WMExchnagerQuerys"][0].query.map(e=>e['$']).map(e=>select(e,'id,querydate,amountin,amountout'));
     var cb=(err,obj)=>g(f(obj));
