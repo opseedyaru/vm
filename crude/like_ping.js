1f715b16fd462355c1fb28c46087047a584b14c4
Adler3D <adler3d@gmail.com>
2018-03-26 23:01:11 +0300

    upd

--- crude/like_ping.js
+++ crude/like_ping.js
@@ -1,4 +1,3 @@
-var protocol='s' in qp?"https://":"http://";
 var c=g_conf_info;
 resp_off();
 var code=("var unixtime=()=>(new Date()/1000);var t=unixtime()-parseFloat(POST.unixtime);return (t*1e3).toFixed(1)+' ms';");
@@ -6,5 +5,5 @@ var path="/eval?&nolog";
 var out_func=jstable_right;
 if('json' in qp)out_func=arr=>txt(json(arr));
 safe_promise_all(
-  mapkeys(c.vh2host).map(e=>axhr_post(protocol+c.vh2host[e]+path,{unixtime:unixtime(),code:code},{vhost:e,host:c.vh2host[e],bef_ms:get_ms()}).then(e=>mapaddfront(e,{ms:get_ms()-e.ud.bef_ms})))
+  mapkeys(c.vh2host).map(e=>axhr_post(with_protocol(c.vh2host[e])+path,{unixtime:unixtime(),code:code},{vhost:e,host:c.vh2host[e],bef_ms:get_ms()}).then(e=>mapaddfront(e,{ms:get_ms()-e.ud.bef_ms})))
 ).then(arr=>out_func(arr.map(e=>{return {vhost:e.ud.vhost,host:e.ud.host,ms:e.ms.toFixed(3),data:e.data,bef_ms:e.ud.bef_ms.toFixed(3)}})));
