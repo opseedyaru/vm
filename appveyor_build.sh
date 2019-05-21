4c5cb762feb514350591b7b34844e89b4dd357fc
adler3d <adler3d@gmail.com>
2019-05-21 03:07:13 +0300

    Update appveyor_build.sh

--- appveyor_build.sh
+++ appveyor_build.sh
@@ -3,7 +3,8 @@ g++ -std=c++14 -O2 proc_mem_limit_detector.cpp -o mem_detect.out
 g++ -std=c++14 -O2 -pthread bq_perf_test.cpp -o bq_perf_test.out
 
 wget https://raw.githubusercontent.com/adler3d/simple_cpp11_vm_like_x86/master/cpu_cycles_per_cmd.cpp
-g++ -std=c++14 -O2 -pthread cpu_cycles_per_cmd.cpp -o cpu_cycles_per_cmd.out
+g++ -DUSE_SSD_MEM -std=c++14 -O2 -pthread cpu_cycles_per_cmd.cpp -o cpu_cycles_per_cmd_ssd.out
+g++ -DUSE_DEF_MEM -std=c++14 -O2 -pthread cpu_cycles_per_cmd.cpp -o cpu_cycles_per_cmd_mem.out
 
 cp *.out artifacts/
 
