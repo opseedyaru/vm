d5f5b936e8de2369490abb5918a6724bef9c609a
adler3d <adler3d@gmail.com>
2019-05-25 13:40:09 +0300

    Update bq_perf_test.cpp

--- bq_perf_test.cpp
+++ bq_perf_test.cpp
@@ -35,7 +35,7 @@ struct bq
 double sec(){struct timeval tv;gettimeofday(&tv,NULL);return tv.tv_sec+tv.tv_usec*1e-6;}
 template<class TYPE>
 void foo(string name,TYPE test_val){
-  bq<TYPE> q0,q1;int n=2*1000*1000;
+  bq<TYPE> q0,q1;int n=2*1000*10;
   auto bef=sec();
   thread t0([&](){
     for(int i=0;i<n;i++){
@@ -54,10 +54,30 @@ void foo(string name,TYPE test_val){
   auto perf=double(n)/t;
   cout<<name<<":{sec:"<<t<<","<<"perf:"<<perf<<"},"<<endl;
 }
+template<class TYPE>
+void raw(string name,TYPE test_val){
+  std::deque<TYPE> q0;auto&q1=q0;int n=20*1000*1000;
+  auto bef=sec();
+  {
+    #define push push_front
+    #define pop pop_back
+    for(int i=0;i<n;i++){
+      q1.push(test_val);
+      auto s=q1.back();q1.pop();
+      q0.push(s);
+      assert(q0.back()==test_val);q0.pop();
+    }
+  }
+  auto t=(sec()-bef);
+  auto perf=double(n)/t;
+  cout<<name<<":{sec:"<<t<<","<<"perf:"<<int(perf)<<"},"<<endl;
+}
 int main(){
   cout<<"{"<<endl;;
   foo<string>("string","test");
   foo<int>("int",9000);
+  raw<string>("raw_string","test");
+  raw<int>("raw_int",9000);
   cout<<"}"<<endl;;
   return 0;
 }
