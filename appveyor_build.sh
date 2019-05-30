db9210e92fae766eaa0ed07a21450ee13df70584
adler3d <adler3d@gmail.com>
2019-05-30 17:25:46 +0300

    Update appveyor_build.sh

--- appveyor_build.sh
+++ appveyor_build.sh
@@ -1,6 +1,6 @@
 if [[ "$1" ==  "run" ]];then
   echo "runned without g++"
-  wget https://ci.appveyor.com/api/buildjobs/bfltbqn9rxfin3tl/artifacts/artifacts.zip
+  wget https://ci.appveyor.com/api/projects/adler3d/vm/artifacts/artifacts.zip
   unzip artifacts.zip
   cp artifacts/*.out ./
   export folder="local_artifacts"
