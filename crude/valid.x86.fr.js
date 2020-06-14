7e1b238c9a5d8c406d7133d6970d4f7d122eab45
adler3d <adler3d@gmail.com>
2020-06-14 19:52:34 +0300

    Update valid.x86.fr.js

--- crude/valid.x86.fr.js
+++ crude/valid.x86.fr.js
@@ -1,7 +1,8 @@
-var site='http://valid.x86.fr/';
-execSync("curl http://valid.x86.fr/rzyidw>valid.x86.fr.html");
+var site='http://valid.x86.fr/';var folder="valid.x86.fr";
+execSync("mkdir -p valid.x86.fr");
+execSync("curl http://valid.x86.fr/rzyidw>"+folder+"/index.html");
 var off='style="display:none"';
-var s=(fs.readFileSync("valid.x86.fr.html")+"").split("<script").join('<nahuy '+off).split("</script>").join("</nahuy>");
+var s=(fs.readFileSync(folder+"/index.html")+"").split("<script").join('<nahuy '+off).split("</script>").join("</nahuy>");
 s=s.split('<section id="main__header" role="banner">').join('<section '+off+'>');
 var m={};
 var is_normext=e=>"css,jpg,gif,svg,png".split(",").map(k=>e.includes("."+k)?"1":"").join("").length;
@@ -13,11 +14,11 @@ var arr2out=(prefix,a,b,arr)=>arr.filter(is_normext).map(e=>{m[e]={
   prefix,a,b
 };return e;}).join('\n');
 var mk_arr=(prefix,a,b)=>arr2out(prefix,a,b,s.split(prefix).slice(1).map(e=>e.split('"')[0]));
-var arr0=mk_arr('"http://','http://','');
-var arr1=mk_arr('<img src="/','','');
+var arr0=mk_arr('"http://','http://','http://vm51.herokuapp.com/'+folder+'/');
+var arr1=mk_arr('<img src="/','/','http://vm51.herokuapp.com/'+folder+'/');
 var arr=[...arr0,...arr1];
 //return
-mapkeys(m).map(e=>"curl "+site+m[e].raw+">./"+m[e].local).map(e=>execSync(e)).join("\n");
+//mapkeys(m).map(e=>"curl "+site+m[e].raw+">./"+folder+"/"+m[e].local).map(e=>execSync(e)).join("\n");
 //return [json(m),out].join("\n");//inspect(out);
 var h=s;mapkeys(m).map(e=>h=h.split(m[e].a+m[e].in_src).join(m[e].b+m[e].local));
 //return txt(inspect(m)+h);
