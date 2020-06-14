8145795be5866a2f06ed8d7bcb0aca54974acfd2
adler3d <adler3d@gmail.com>
2020-06-14 20:41:44 +0300

    Update valid.x86.fr.js

--- crude/valid.x86.fr.js
+++ crude/valid.x86.fr.js
@@ -1,9 +1,10 @@
-var href="http://valid.x86.fr";if('href' in qp)href=qp.href;if('url' in qp)href=qp.url;
+var href="https://habr.com/ru/top/";if('href' in qp)href=qp.href;if('url' in qp)href=qp.url;
 var hp=url.parse(href);
 if(!hp.protocol||!hp.protocol.includes("http"))return "protocol - required in:"+inspect(hp);
+var hpp=hp.protocol+'//';
 var site=hp.protocol+"//"+url.parse(href).host+'/';var folder=hp.host;var host=request.headers.host;
 execSync("mkdir -p "+folder);
-execSync("curl http://valid.x86.fr/rzyidw>"+folder+"/index.html");
+execSync("curl "+href+">"+folder+"/index.html");
 var off='style="display:none"';
 var s=(fs.readFileSync(folder+"/index.html")+"").split("<script").join('<nahuy '+off).split("</script>").join("</nahuy>");
 s=s.split('<section id="main__header" role="banner">').join('<section '+off+'>');
@@ -19,10 +20,11 @@ var arr2out=(prefix,a,b,arr)=>arr.filter(is_normext).map(e=>{m[e]={
 var mk_arr=(prefix,a,b)=>arr2out(prefix,a,b,s.split(prefix).slice(1).map(e=>e.split('"')[0]));
 var b='http://'+host+'/'+folder+'/';
 var arr0=mk_arr('"http://','http://',b);
+var arr0=mk_arr('"https://','https://',b);
 var arr1=mk_arr('<img src="/','/',b);
 var arr=[...arr0,...arr1];
 //return
-mapkeys(m).map(e=>"curl "+site+m[e].raw+">./"+folder+"/"+m[e].local).map(e=>execSync(e)).join("\n");
+//mapkeys(m).map(e=>"curl "+site+m[e].raw+">./"+folder+"/"+m[e].local).map(e=>execSync(e)).join("\n");
 //return [json(m),out].join("\n");//inspect(out);
 var h=s;mapkeys(m).map(e=>h=h.split(m[e].a+m[e].in_src).join(m[e].b+m[e].local));
 //return txt(inspect(m)+h);
