262f98d9c4ac41fafcc32dfcc36e404ae9082262
adler3d <adler3d@gmail.com>
2020-07-08 19:21:02 +0300

    Update appveyor_build.sh

--- appveyor_build.sh
+++ appveyor_build.sh
@@ -8,20 +8,20 @@ if [[ "$1" ==  "run" ]];then
   export folder="local_artifacts"
   mkdir -p $folder
 else
-echo required os = ubuntu 1604
-g++ -std=c++14 -O2 proc_mem_limit_detector.cpp -o mem_detect.out
-g++ -std=c++14 -O2 -pthread bq_perf_test.cpp -o bq_perf_test.out
+  echo required os = ubuntu 1604
+  g++ -std=c++14 -O2 proc_mem_limit_detector.cpp -o mem_detect.out
+  g++ -std=c++14 -O2 -pthread bq_perf_test.cpp -o bq_perf_test.out
 
-wget https://raw.githubusercontent.com/adler3d/simple_cpp11_vm_like_x86/master/cpu_cycles_per_cmd.cpp
-g++ -DUSE_SSD_MEM -std=c++14 -O2 -pthread cpu_cycles_per_cmd.cpp -o cpu_cycles_per_cmd_ssd.out
-g++ -DUSE_DEF_MEM -std=c++14 -O2 -pthread cpu_cycles_per_cmd.cpp -o cpu_cycles_per_cmd_mem.out
+  wget https://raw.githubusercontent.com/adler3d/simple_cpp11_vm_like_x86/master/cpu_cycles_per_cmd.cpp
+  g++ -DUSE_SSD_MEM -std=c++14 -O2 -pthread cpu_cycles_per_cmd.cpp -o cpu_cycles_per_cmd_ssd.out
+  g++ -DUSE_DEF_MEM -std=c++14 -O2 -pthread cpu_cycles_per_cmd.cpp -o cpu_cycles_per_cmd_mem.out
 
-export folder="artifacts"
-mkdir -p $folder
-cp *.out $folder/
-echo nope>$folder/os_js.txt
-echo nope>$folder/appveyor_serv_info.txt
-echo nope>$folder/si.json
+  export folder="artifacts"
+  mkdir -p $folder
+  cp *.out $folder/
+  echo nope>$folder/os_js.txt
+  echo nope>$folder/appveyor_serv_info.txt
+  echo nope>$folder/si.json
 fi
 
 serv_info() {
@@ -36,17 +36,16 @@ serv_info() {
 
   echo "os_info"
   node crude/os.js|tee $folder/os_js.txt
-  echo "more_lulz:{ssd:"
+  echo $'more_lulz:{\"ssd\":'
   ./cpu_cycles_per_cmd_ssd.out
-  echo ","
-  echo "mem:"
+  echo $',\n\"mem\":'
   ./cpu_cycles_per_cmd_mem.out
-  echo ","
-  echo "bq_perf_test_all:"
+  echo $',\n\"bq_perf_test_all\":'
   ./bq_perf_test.out
-  echo ","
-  echo "bq_perf_test_one:"
+  echo $',\n\"bq_perf_test_one\":'
   taskset -c 0 ./bq_perf_test.out
+  echo $',\n\"mem_detect\":'
+  ./mem_detect.out
   echo "}:more_lulz"
 }
 serv_info>$folder/appveyor_serv_info.txt
